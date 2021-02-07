dc7e52dd548b6751c76d08fdb6797237acd5ca1d
lokeshg-fsd <lokesh.jejappa@gmail.com>
2021-02-08 00:35:25 +0530

    add ESLint Prettier and Flow To the setup

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -1,3 +1,4 @@
+// @flow
 // Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
 // like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
 // of the page.
@@ -6,21 +7,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
-
+const Hello = (props) => <div>Hello {props.name}!</div>
 Hello.defaultProps = {
-  name: 'David'
+    name: 'David',
 }
 
 Hello.propTypes = {
-  name: PropTypes.string
+    name: PropTypes.string,
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <Hello name="React" />,
-    document.body.appendChild(document.createElement('div')),
-  )
+    ReactDOM.render(
+        <Hello name="React" />,
+        document.body.appendChild(document.createElement('div')),
+    )
 })
