4deb0ad265dcf4f7b1f0ce078488ddc609352dc1
Lokesh G <77382261+lokeshgss@users.noreply.github.com>
2021-05-30 20:36:16 +0530

    Revert "Add graphql to fetch api data"

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,2 @@
-// @flow
-
-// = link_tree ../images
-// = link_directory ../stylesheets .css
-// = link graphiql/rails/application.css
-// = link graphiql/rails/application.js
+//= link_tree ../images
+//= link_directory ../stylesheets .css
