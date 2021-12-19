7347ff0bf7bd7b28fcea83213e51bc48557a3b1e
lokeshgss <lokesh.gudisinti@softsuave.com>
2021-12-19 16:03:11 +0530

    [deploy Passed heyyy:@]

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   def index
     @page_title = 'Saver Home'
   end
